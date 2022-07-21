11fc41fd07c5b0126bf6bb789cec50a853784f9e
Anri <anri0806@hotmail.co.jp>
2022-07-21 14:58:11 -0400

    removed unneccessary codes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,5 @@
 class SessionsController < ApplicationController
 
-    def show
-        user = User.find_by(id: session[:user_id])
-        if user
-            render json: user
-        else
-            render json: { error: "Not authorized"}, status: :unauthorized
-        end
-    end
-
     def create
         user = User.find_by(username: params[:username])
         if user&.authenticate(params[:password])
